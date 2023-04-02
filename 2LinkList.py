4da79d1bb43c5681435fad8f20f0ebac455bfc45
Petr Alekseev <petr.alekseev@ntprog.com>
2023-04-02 20:24:09 +0300

    Done

--- 2LinkList.py
+++ 2LinkList.py
@@ -1,9 +1,10 @@
 class Node:
     def __init__(self, data=None):
         self.data = data
+        self.prev = None
         self.next = None
 
-class LinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
@@ -16,10 +17,13 @@ class LinkedList:
         while current_node.next is not None:
             current_node = current_node.next
         current_node.next = new_node
+        new_node.prev = current_node
 
     def prepend(self, data):
         new_node = Node(data)
         new_node.next = self.head
+        if self.head is not None:
+            self.head.prev = new_node
         self.head = new_node
 
     def delete_node(self, data):
@@ -27,11 +31,14 @@ class LinkedList:
             return
         if self.head.data == data:
             self.head = self.head.next
+            self.head.prev = None
             return
         current_node = self.head
         while current_node.next is not None:
             if current_node.next.data == data:
                 current_node.next = current_node.next.next
+                if current_node.next is not None:
+                    current_node.next.prev = current_node
                 return
             current_node = current_node.next
 
@@ -40,10 +47,3 @@ class LinkedList:
         while current_node is not None:
             print(current_node.data)
             current_node = current_node.next
-
-if __name__ == '__main__':
-    a = LinkedList()
-    a.append(1)
-    a.append(2)
-    a.prepend(3)
-    a.print_list()
